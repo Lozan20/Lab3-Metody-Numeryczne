e19b5e4833ec9ba14c742921723fccd9f5ed18e5
Lozan <lozanskimichal@gmail.com>
2020-04-14 20:46:51 +0200

    calkowanie wzorem parabol i trapezow dla roznych przedzialow

--- Calkowanie Numeryczne/Calkowanie Numeryczne.cpp
+++ Calkowanie Numeryczne/Calkowanie Numeryczne.cpp
@@ -1,20 +1,59 @@
-﻿// Calkowanie Numeryczne.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
 
-#include <iostream>
+double trapezy(double a, double b,double przedzialy)
+{
+    double odstep = (b - a) / przedzialy;
+    double suma = 0;
+    for (int i = 0; i < przedzialy; i++)
+    {
+        suma += ((((i + 1.0) * odstep) - (i * odstep)) / 2) * (1 / (1 + 2 * (i * odstep)) + (1 / (1 + 2 * ((i + 1.0) * odstep))));
+
+    }
+    return suma;
+}
+
+double parabole(double a, double b,double przedzialy)
+{
+    double odstep = (b - a) / przedzialy;
+    double suma = 0;
+    for (int i = 0; i < przedzialy; i++)
+    {
+        suma += (((i + 1.0) * odstep) - (i * odstep)) / 6 * (1 / (1 + 2 * (i * odstep)) + 4 * (1 / (1 + 2 * ((i * odstep + ((i + 1.0) * odstep)) / 2))) + (1 / (1 + 2 * ((i + 1.0) * odstep))));
+    }
+    return suma;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    int choice;  
+    std::cout << "1. Calkowanie za pomoca wzoru trapezow" << std::endl
+        << "2. Calkowanie za pomoca wzoru parabol" << std::endl
+        << "3. Calkowanie za pomoca wzoru trapezow dla 10 przedzialow" << std::endl
+        << "4. Stopniowe zwiekszanie przedzialow wzorem trapezow" << std::endl
+        << "5. Stopniowe zwiekszanie przedzialow wzorem parabol" << std::endl;
+    std::cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        std::cout << trapezy(0,2,1);
+        break;
+    case 2:
+        std::cout << parabole(0, 2,1);
+        break;
+    case 3:
+        std::cout << trapezy(0, 2, 10);
+        break;
+    case 4:
+        for (int i = 1; i <= 10; i++)
+            std::cout << "Liczba Przedzialow: " << i << " Wynik: " << trapezy(0, 2, i) << std::endl;
+        break;
+    case 5:
+        for (int i = 1; i <= 10; i++)
+            std::cout << "Liczba Przedzialow: " << i << " Wynik: " << parabole(0, 2, i) << std::endl;
+        break;
+    }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
+}   
+
+
